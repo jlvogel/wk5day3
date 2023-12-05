4d530c2c87fdef35f7e2823263d4d06c39c11554
Joshua Vogel <40187940+jlvogel@users.noreply.github.com>
2023-12-05 11:40:54 -0500

    CSS Flexbox and CSS Grid practice

---
+++ script.js
@@ -0,0 +1,65 @@
+// Section 1
+// intro to Flexbox
+
+// Learning Objectives
+
+//  Students Wil Be Able To:
+//    Describe the Use Case of Flexbox
+//    Use Flexbox for One-Dimensional Layout
+
+// Roadmap
+//  Setup
+//  Intro to Flexbox & CSS Grid
+//  Flexbox Fundamentals
+//  Your First Flexbox
+
+// Intro to Flexbox & CSS Grid
+
+//  As a front-end developer, you will be required to precisely
+//  layout the elements on web pages.
+
+//  Prior to Flexbox & CSS Grid, laying out the parts of a web
+//  page from basic navigation headers to complex full-page
+//  layouts has not been as straightforward as it could be -
+//  Flexbox & Grid, however, are game changers.
+
+//  The capabilities of Flexbox & CSS Grid complement each
+//  other and using both side-by-side and even nesting one
+//  within the other is possible.
+
+//  The difference between Flexbox and CSS Grid is how they
+//  are designed to lay out their children:
+
+// Flexbox: One Dimension
+
+// CSS Grids: Two Dimensions
+
+// Flexbox
+
+// Why Use Flexbox?
+
+//  Flexbox excels at assisting devs with the following tasks:
+
+//    Vertically centering content & elements within a container
+//    element
+
+//    Spacing child elements within a container uniformly
+
+//    Making the height of child elements laid out in columns
+//    the same even though they have a different amount of content.
+
+// Flexbox Fundamentals
+
+// We use a CSS display: flex; declaration to make an
+// element a flex container, for example:
+
+//    section {
+//      display: flex;
+//    }
+
+//  The above would make all <section> elements flex
+//  containers and all direct children become flex items.
+
+//  Let's open a separate tab and briefly review what has
+//  become the de facto guide to Flexbox:
+//  https://css-tricks.com/snippets/css/a-guide-to-flexbox/
